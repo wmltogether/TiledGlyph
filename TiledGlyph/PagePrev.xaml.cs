a34c2a527766d0708bbbdba3047b7c988145e5d1
wmltogether <wmltogether@gmail.com>
2015-12-25 13:23:07 +0800

    Add Show Tile Grid feature

--- TiledGlyph/PagePrev.xaml.cs
+++ TiledGlyph/PagePrev.xaml.cs
@@ -48,6 +48,36 @@ namespace TiledGlyph
                         image.Height = cbmp.Height;
                         image.Width = cbmp.Width;
                         image.Source = cbmp;
+                        if (checkboxShowLine.IsChecked == true)
+                        {
+                            DrawingVisual drawingVisual = new DrawingVisual();
+                            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+                            {
+                                //
+                                // ... draw on the drawingContext
+                                //
+                                System.Windows.Media.Pen pen = new System.Windows.Media.Pen(System.Windows.Media.Brushes.Red, (double)0.5f);
+                                drawingContext.DrawImage(image.Source, new Rect(0, 0, image.Source.Width, image.Source.Height));
+                                int x_lines = (int)Math.Round((float)image.Source.Width / (float)GlobalSettings.iTileWidth);
+
+                                int y_lines = (int)Math.Round((float)image.Source.Height / (float)GlobalSettings.iTileHeight); ;
+                                for (int i = 0; i < x_lines + 1; i++)
+                                {
+                                    drawingContext.DrawLine(pen, new System.Windows.Point(i * GlobalSettings.iTileWidth, 0),
+                                        new System.Windows.Point(i * GlobalSettings.iTileWidth, image.Source.Height));
+                                }
+                                for (int i = 0; i < y_lines + 1; i++)
+                                {
+                                    drawingContext.DrawLine(pen, new System.Windows.Point(0, i * GlobalSettings.iTileHeight),
+                                        new System.Windows.Point(image.Source.Height, i * GlobalSettings.iTileHeight));
+                                }
+                                drawingContext.Close();
+                                RenderTargetBitmap nbmp = new RenderTargetBitmap((int)image.Width, (int)image.Height, 96.0, 96.0, PixelFormats.Default);
+                                nbmp.Render(drawingVisual);
+                                image.Source = nbmp;
+                            }
+                        }
+
                     })
                );
             });
@@ -67,6 +97,16 @@ namespace TiledGlyph
             }
 
             return bitmapImage;
+        }
+
+        private void checkboxShowLine_Unchecked(object sender, RoutedEventArgs e)
+        {
+            AsynDisplayImage();
+        }
+
+        private void checkboxShowLine_Checked(object sender, RoutedEventArgs e)
+        {
+            AsynDisplayImage();
         }
     }
 }
